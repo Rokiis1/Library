a806b2ff6c5eef85f3c5bbe3d8b1ed7d2f7c54a9
Rokas Norvilis <rokasnorviliss@.com>
2023-01-18 11:05:14 +0200

    Added validation to register

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel as PydanticBaseModel
+from pydantic import BaseModel as PydanticBaseModel, validator
 
 
 class User(PydanticBaseModel):
@@ -8,3 +8,16 @@ class User(PydanticBaseModel):
     hashed_password: str
     repeat_password: str
     is_active: bool = True
+    
+    
+    @validator("username", pre=True)
+    def username_min_length(cls, value):
+        if value and len(value) < 6:
+            raise ValueError("Username must be at least 6 characters long.")
+        return value
+
+    @validator("hashed_password", pre=True)
+    def password_min_length(cls, value):
+        if len(value) < 8:
+            raise ValueError("Password must be at least 8 characters long.")
+        return value
